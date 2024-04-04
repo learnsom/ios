833eb9711b47e1f3f43e227682bd2d25b0017db1
learnsom <166076409+learnsom@users.noreply.github.com>
2024-04-05 09:50:05 +1030

    Create app.js
    Signed-off-by: learnsom <166076409+learnsom@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "iosTools",
+  "website": "https://twitter.com/iosTools",
+  "description": "Love iOS apps",
+  "logo": "https://raw.githubusercontent.com/learnsom/ios/master/Nextcloud%20Hub.png",
+  "twitter": "https://twitter.com/iosTools"
+}
